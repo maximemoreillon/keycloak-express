6c189b15b61ea607c6d7bb787c8a105ee6bb96d5
Maxime Moreillon <m.moreillon@gmail.com>
2024-09-11 12:55:26 +0900

    externalized oidc middleare

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Keycloak + Express
 
 This is sample code to demonstrate how to authenticate an Express API with Keycloak.
-Normally, the keycloak-connect could be used but as it is now deprecated, this sample also includes a method using the openid-client module.
+Normally, the keycloak-connect could be used but as it is now deprecated, this sample also includes a method using the [@moreillon/express-oidc](https://www.npmjs.com/package/@moreillon/express-oidc) module, which is a wrapper adound [openid-client](https://www.npmjs.com/package/openid-client) .
--- index.ts
+++ index.ts
@@ -1,14 +1,26 @@
 import express, { type Request, type Response } from "express"
 import cors from "cors"
 // import { middleware as keycloakConnectMiddleware } from "./keycloak"
-import { introspectMiddleware, userInfoMiddleware } from "./oidc"
+import {
+  introspectMiddleware,
+  userInfoMiddleware,
+} from "@moreillon/express-oidc"
+
+const { KEYCLOAK_ISSUER_URL, KEYCLOAK_CLIENT_SECRET, KEYCLOAK_CLIENT_ID } =
+  process.env
 
 const app = express()
 
 app.use(cors())
 
 // app.use(introspectMiddleware)
-app.use(userInfoMiddleware)
+app.use(
+  userInfoMiddleware({
+    issuer_url: KEYCLOAK_ISSUER_URL,
+    client_id: KEYCLOAK_CLIENT_ID,
+    client_secret: KEYCLOAK_CLIENT_SECRET,
+  })
+)
 
 const handler = (req: Request, res: Response) => {
   console.log("GET /data")
